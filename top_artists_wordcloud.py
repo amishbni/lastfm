0bf1364852c44f1fe4e1521efa06554e67ff326e
Amir A. Shabani <amirshabani011@gmail.com>
2020-11-25 09:49:28 +0330

    Choose colormap randomly

--- top_artists_wordcloud.py
+++ top_artists_wordcloud.py
@@ -1,5 +1,6 @@
 from os import path
 from wordcloud_fa import WordCloudFa
+from random import choice
 import pandas as pd
 import arabic_reshaper as ar
 import re
@@ -18,10 +19,15 @@ data = pd.read_csv(csv_address, header=None, names=columns)
 top_series = data["artist"].value_counts()[:50]
 top = dict(zip(top_series.index, top_series))
 
+colormaps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink', 'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper']
+
+colormap = choice(colormaps)
+print(f"Colormap: {colormap}")
+
 wordcloud = WordCloudFa(
 	prefer_horizontal=1,
 	background_color="white",
-	colormap="magma",
+	colormap=colormap,
 	width=2048,
 	height=2048
 ).generate_from_frequencies(top)
